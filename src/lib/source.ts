f60f017995836d93725a139490bc4eb376426ef1
kWAY <55437151+kWAYTV@users.noreply.github.com>
2025-05-12 09:54:18 +0200

    feat(docs): add icons to documentation pages
    - Added icon metadata to index.mdx and requirements.mdx for improved visual representation.
    - Enhanced the source loader in source.ts to support dynamic icon rendering based on provided icon names.

--- src/lib/source.ts
+++ src/lib/source.ts
@@ -1,10 +1,20 @@
 import { loader } from 'fumadocs-core/source';
+import { icons } from 'lucide-react';
 
 import { docs } from '@/.source';
+import { createElement } from 'react';
 
 // See https://fumadocs.vercel.app/docs/headless/source-api for more info
 export const source = loader({
   // it assigns a URL to your pages
   baseUrl: '/docs',
-  source: docs.toFumadocsSource()
+  source: docs.toFumadocsSource(),
+  icon(icon) {
+    if (!icon) {
+      // You may set a default icon
+      return;
+    }
+
+    if (icon in icons) return createElement(icons[icon as keyof typeof icons]);
+  }
 });
